ca979386c8548008717ebd4d4f0cb94db5499cd2
Roshanell Francisco <>
2023-02-24 10:58:46 -0600

    updated routes

--- index.js
+++ index.js
@@ -16,18 +16,25 @@ app.get("/", (req, res) => {
 
 app.get("/create-books", (req, res) => {
 	res.sendFile(path.join(__dirname, "./new-book.html"));
+	console.log(req.body);
 	// res.send("Book created successfully!");
 });
 
-app.post("/books", (req, res) => {
-	console.log(req.body);
-	res.redirect("/all-books");
-	// res.sendFile(__dirname, "./new-book.html");
-});
+// app.get("/books", (req, res) => {
+// 	res.send()
+// });
+
+// app.post("/books", (req, res) => {
+// 	console.log(req.body);
+// 	res.redirect("/all-books");
+
+// 	// res.sendFile(__dirname, "./new-book.html");
+// });
 
 app.get("/all-books", (req, res) => {
 	console.log("hi");
 	res.send("list of books go here");
+	console.log(req.body);
 });
 app.listen(port, () =>
 	console.log(`Server is running on ${port}.You better go catch it!`)
